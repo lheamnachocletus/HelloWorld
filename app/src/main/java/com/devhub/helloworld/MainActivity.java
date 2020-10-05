6db933e9a28f9d363f4c481eb0fd54e6b56e113d
lheamnachocletus <iheamnachoebulue@gmail.com>
2020-10-05 16:26:14 +0300

    text changing code done on button click

--- app/src/main/java/com/devhub/helloworld/MainActivity.java
+++ app/src/main/java/com/devhub/helloworld/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn1=findViewById(R.id.btn1);
-        btn2=findViewById(R.id.btn2);
+
         textView=findViewById(R.id.textview);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,12 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+      
 
-                textView.setTextColor(Color.GREEN);
-            }
-        });
     }
 }
